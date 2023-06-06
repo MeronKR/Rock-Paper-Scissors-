92439236e1d11b55ad97ef1892cc6c55dcaef811
MeronKR <134378150+MeronKR@users.noreply.github.com>
2023-06-06 16:42:55 +0900

    Complete function to play a round

--- apps.js
+++ apps.js
@@ -1,5 +1,45 @@
+const option = ["rock", "paper", "scissor"]
+
 function getComputerChoice(){
-    let option = ["rock", "paper", "scissors"]
     let choice = option[Math.floor(Math.random()*option.length)]
+    console.log(choice)
     return choice;
 }
+
+function checkWinner(playerSelection,computerSelection){
+    if (playerSelection === computerSelection){
+        return "Tie";}
+        
+        else if(
+            (playerSelection == "rock" && computerSelection == "scissor") ||
+            (playerSelection == "paper" && computerSelection == "rock") ||
+            (playerSelection == "scissor" && computerSelection == "paper")
+        ){
+                return "Player";}
+        else {
+            (playerSelection == "rock" && computerSelection == "paper") ||
+            (playerSelection == "paper" && computerSelection == "scissor") ||
+            (paperSelection = "scissor" && computerSelection == "rock")
+            return "Computer";
+        }
+}   
+
+function playRound(playerSelection, computerSelection) {
+    let result = checkWinner(playerSelection, computerSelection);
+
+    if (result ==="Tie"){
+        return "Tie! Play again!"
+    }
+
+    else if (result == "Player"){
+        return `You win! ${playerSelection} beats ${computerSelection}`
+    }
+
+    else {
+        return `You lose! ${computerSelection} beats ${playerSelection}`
+    }   
+}
+
+const playerSelection = "paper";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
