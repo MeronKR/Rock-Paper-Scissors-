8eec94c939ad17055bab15cd56089b22928ad84b
MeronKR <134378150+MeronKR@users.noreply.github.com>
2023-06-06 15:26:14 +0900

    Rework getComputerChoice function with array

--- apps.js
+++ apps.js
@@ -1,18 +1,5 @@
-/* Assign random pc value */
-    function getComputerChoice(){
-        const randNum = (Math.floor(Math.random()*3 + 1));
-        let computerSelect = "";
-        console.log(computerSelect)
-        if (randNum === 1) {
-         return computerSelect = "rock"
-         console.log(computerSelect);
-        } else if(randNum === 2) {
-         return computerSelect = "paper"
-         console.log(computerSelect);
-     }   else {
-          return computerSelect = "scissor"
-         console.log(computerSelect);
-     }
-     }
-
-    
+function getComputerChoice(){
+    let option = ["rock", "paper", "scissors"]
+    let choice = option[Math.floor(Math.random()*option.length)]
+    return choice;
+}
