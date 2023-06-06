34f7a653e04c249f039abdbcf3bfa69aaa198ec7
MeronKR <134378150+MeronKR@users.noreply.github.com>
2023-06-06 12:53:17 +0900

    Add assignment of computer select value

--- apps.js
+++ apps.js
@@ -1,14 +1,18 @@
 /* Assign random pc value */
+    function getComputerChoice(){
+        const randNum = (Math.floor(Math.random()*3 + 1));
+        let computerSelect = "";
+        console.log(computerSelect)
+        if (randNum === 1) {
+         return computerSelect = "rock"
+         console.log(computerSelect);
+        } else if(randNum === 2) {
+         return computerSelect = "paper"
+         console.log(computerSelect);
+     }   else {
+          return computerSelect = "scissor"
+         console.log(computerSelect);
+     }
+     }
 
-function getComputerChoice(){
-   const randNum = (Math.floor(Math.random()*3 + 1));
-   if (randNum === 1) {
-    console.log("Rock")
-   } else if(randNum === 2) {
-    console.log("Paper")
-}   else if (randNum === 3) {
-    console.log("Scissor")
-}
-}
-
-
+    
